3e79a7fdabcbe7f67405e361c848b30282c5fd22
maddygbutler <madelinebutler@students.berkeley.netgit confH205-MM24:~ h205p5$ git config --global user.email madelinebutler@studeeley.net>
2019-04-25 13:57:09 -0700

    hey

--- script.js
+++ script.js
@@ -11,21 +11,30 @@ $(document).ready(function(){
         //     success:news
         // });
 
-
+        var country= $("#countries").val();
+        var topic= $("#topic").val();
         $.ajax({
-            url: 'https://newsapi.org/v2/top-headlines?country=us&apiKey=c46f84f1e83a472f825752fba49526d4',
+
+            url: 'https://newsapi.org/v2/top-headlines?country=' + country+ '&category=' + topic +'&apiKey=c46f84f1e83a472f825752fba49526d4',
             dataType: 'JSON',
             jsonpCallback: 'callback',
             type: 'GET',
             success: function (data) {
                 console.log(data);
                 var headlines=$("#headlines").val();
+
                 for(var i =0; i < headlines;i++){
+
                     var title=data.articles[i].title;
                     var dis=data.articles[i].description;
                     var url=data.articles[i].url;
+                    var str= "  Read the Full Article";
+                    var link= str.link( url);
+
                     $("#news").append(title+ "<br>");
                     $("#news").append("  " + dis + "<br><br>");
+                    $("#news").append(link + "<br>");
+
 
                 }
 
